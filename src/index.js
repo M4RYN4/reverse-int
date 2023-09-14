1a6beb1d804107bc9ae79a03d91ffc163e4676d7
Maryna <rimana.ca@gmail.com>
2023-09-14 12:00:24 -0400

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+//num to str, str to arr, reverse arr, arr back to str, str to num
+//convert num to array
+  let str = Math.abs(n).toString();//as the n have negative values
+  let arr = str.split('');
+  let reversedArr = arr.reverse();
+  //& back
+  let str2 = reversedArr.join('');
+  let num = parseInt(str2);
+  return num;
 }
